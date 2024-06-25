956961a90533583ae89de5dda1ecf71d153cbb02
Kolovratik <Kolovratik@HOME-PC>
2024-06-25 18:58:03 +0300

    Создание страницы графика

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,10 +9,12 @@ namespace PR53
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
         public List<PointInfo> pointsInfo = new List<PointInfo>();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
         }
     }
 }
